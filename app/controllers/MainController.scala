6b7b04bf3e07471bc1ddcd7467ecae88929699d5
Brian Clapper <bmc@clapper.org>
2014-07-01 22:20:08 -0400

    Refactored getAngularTemplate() action.

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -7,12 +7,17 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.Play.current
 
 import scala.concurrent.Future
+import scala.util.{Success, Try}
 
 /** Main controller.
   */
 object MainController extends BaseController {
 
   override protected val logger = Logger("controllers.MainController")
+
+  // -------------------------------------------------------------------------
+  // Public methods
+  // -------------------------------------------------------------------------
 
   def index = Action {
     Ok(views.html.index())
@@ -25,6 +30,7 @@ object MainController extends BaseController {
   def static(path: String) = Action.async {
     sendFile(s"static/$path")
   }
+
   /** Retrieve an Angular.js template. Most of the time, these templates
     * could be static. However, every so often, it's useful to fill something
     * in on the backend first. So, we run each one through Play's templating
@@ -32,50 +38,64 @@ object MainController extends BaseController {
     *
     * NOTE: Template names should be specified as `foo.html`, not as
     * `foo.scala.html`, in the Angular.js code. Angular doesn't need to know
-    * that the backend template is being processed by Scala.
+    * whether the backend template is being processed by Scala.
+    *
+    * This action first attempts to find a static template. If a static
+    * template is not available, the action attempts to find a Play
+    * template class.
     *
     * @param name  the template name.
     */
   def getAngularTemplate(name: String) = Action.async {
     Future {
-      import grizzled.file.util
-
       val file = Play.getFile(s"static/AngularTemplates/$name")
-      if (file.exists) {
-        Ok.sendFile(content = file, inline = true)
+      val result = if (file.exists) {
+        Success(Ok.sendFile(content = file, inline = true))
       }
 
       else {
         // Static file doesn't exist. Try loading and processing as a Play
         // template.
 
-        val (dirname, basename, extension) = util.dirnameBasenameExtension(name)
-        val parent = dirname.replaceAllLiterally(".", "").trim()
+        findAngularPlayTemplate(name)
+      }
 
-        val className = if (parent.length > 0)
-          s"views.html.AngularTemplates.$parent.$basename"
-        else
-          s"views.html.AngularTemplates.$basename"
+      result.recover {
+        case e: ClassNotFoundException =>
+          NotFound
 
-        try {
-          val templateClass = Class.forName(className)
-          val applyMethod = templateClass.getMethod("apply")
-          applyMethod.invoke(null) match {
-            case html: play.api.templates.HtmlFormat.Appendable =>
-              Ok(html)
-
-            case r: Any => {
-              logger.error(s"unexpected ${r.getClass} type from template")
-              InternalServerError
-            }
-          }
+        case e: Exception => {
+          logger.error(s"Error loading template for $name", e)
+          InternalServerError
         }
+      }.get
+    }
+  }
 
-        catch {
-          case e: Exception => {
-            logger.error(s"No static or dynamic template for $name", e)
-            NotFound
-          }
+  // -------------------------------------------------------------------------
+  // Private methods
+  // -------------------------------------------------------------------------
+
+  private def findAngularPlayTemplate(name: String): Try[Result] = {
+    import grizzled.file.util
+
+    val (dirname, basename, extension) = util.dirnameBasenameExtension(name)
+    val parent = dirname.replaceAllLiterally(".", "").trim()
+
+    val className = if (parent.length > 0)
+      s"views.html.AngularTemplates.$parent.$basename"
+    else
+      s"views.html.AngularTemplates.$basename"
+
+    Try {
+      val templateClass = Class.forName(className)
+      val applyMethod = templateClass.getMethod("apply")
+      applyMethod.invoke(null) match {
+        case html: play.twirl.api.HtmlFormat.Appendable =>
+          Ok(html)
+
+        case r: Any => {
+          throw new Exception(s"unexpected ${r.getClass} type from template")
         }
       }
     }
